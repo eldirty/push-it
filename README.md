0ede409dceece1f5d047383481a039d2307f5cb6
El Dirty <eldirtydancer@gmail.com>
2015-09-04 20:26:05 +0200

    Going dirty with own fork

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 Installs the 80s classic [Push it](http://www.youtube.com/watch?v=YleXlgHI1oM) by Salt-n-Pepa as
 a Git commit hook the current working directory. Oh, and it cranks the volume to 10. Every second.
 
-    $ curl -s --location http://raw.github.com/jgorset/push-it/master/install.sh | sh
+    $ curl -s --location http://raw.github.com/eldirty/push-it/master/install.sh | sh
 
 # I love you
 
-Johannes Gorset made this. You should [tweet me](http://twitter.com/jgorset>) if you can't get it
+Johannes Gorset made this. You should [tweet me](http://twitter.com/eldirty>) if you can't get it
 to work. In fact, you should tweet me anyway.
--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Downloading song..."
-curl -s --location "https://raw.github.com/jgorset/push-it/master/push-it.mp3" > ~/Music/push-it.mp3
+curl -s --location "https://raw.github.com/eldirty/push-it/master/push-it.mp3" > ~/Music/push-it.mp3
 
 echo "Applying commit-msg hook..."
-curl -s --location "https://raw.github.com/jgorset/push-it/master/commit-msg" > .git/hooks/commit-msg
+curl -s --location "https://raw.github.com/eldirty/push-it/master/commit-msg" > .git/hooks/commit-msg
 chmod +x .git/hooks/commit-msg
 
 echo "Applying pre-push hook..."
-curl -s --location "https://raw.github.com/jgorset/push-it/master/pre-push" > .git/hooks/pre-push
+curl -s --location "https://raw.github.com/eldirty/push-it/master/pre-push" > .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
 
 echo "Done."
